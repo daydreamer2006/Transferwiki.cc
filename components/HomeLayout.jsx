b339ca535b6a36bf7b9b4311c617630b7aeb9185
SHICC <Shicgz@gmail.com>
2025-03-22 01:45:04 -0500

    1
    1

--- components/HomeLayout.jsx
+++ components/HomeLayout.jsx
@@ -4,9 +4,13 @@ import Link from 'next/link';
 
 export default function HomeLayout({ children }) {
   return (
-    <div>
-      <h1>TransferWiki.cc</h1>
-      {children}
+    <div className="container mx-auto px-4">
+      <header className="py-6">
+        <h1 className="text-3xl font-bold">TransferWiki.cc</h1>
+      </header>
+      <main>
+        {children}
+      </main>
     </div>
   );
 }
